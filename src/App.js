5724a71c7c888576b2a82c6de1a7de9e6dfa2c76
fowusu <osei1257@gmail.com>
2024-08-18 23:44:57 +0000

    refactor to avatar function in profile

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import Gallery from "./Gallery";
 import Profile from "./Profile";
 import TodoList from "./TodoList";
 import SecondProfile from "./SecondProfile";
+import Avatar from "./Avatar";
 
 function MyButton() {
-  return (
-    <button>
-      I'm a button
-    </button>
-  );
+  return <button>I'm a button</button>;
 }
 
+function Card({ children }) {
+  return <div className="card">{children}</div>;
+}
 
 export default function MyApp() {
   return (
@@ -23,6 +23,13 @@ export default function MyApp() {
       <SecondProfile />
       <Gallery />
       {/* <MyButton /> */}
+      <h3>Show Card</h3>
+      <Card>
+        <Avatar
+          size={100}
+          person={{ name: "Katsuko Saruhasi", imageId: "Yfe)qp2" }}
+        />
+      </Card>
       <TodoList />
     </div>
   );

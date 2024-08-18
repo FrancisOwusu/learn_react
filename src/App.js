79f0a226f27814c5b5d39c3cfb92f0f9e184e4ed
fowusu <osei1257@gmail.com>
2024-08-18 23:22:34 +0000

    add props to SecondProfile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Gallery from "./Gallery";
 import Profile from "./Profile";
 import TodoList from "./TodoList";
+import SecondProfile from "./SecondProfile";
 
 function MyButton() {
   return (
@@ -15,7 +16,11 @@ export default function MyApp() {
   return (
     <div>
       <h1>Welcome to my app</h1>
+      <h3>First Profile</h3>
       <Profile />
+
+      <h3>Second Profile</h3>
+      <SecondProfile />
       <Gallery />
       {/* <MyButton /> */}
       <TodoList />
--- src/Profile.js
+++ src/Profile.js
@@ -1,7 +1,17 @@
-export default function Profile(){
-    return(
-        <img
-      src="https://i.imgur.com/MK3eW3As.jpg"
-      alt="Katherine Johnson"
-    />)
-    }
+
+function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src="https://i.imgur.com/1bX5QH6.jpg"
+      alt="Lin Lanying"
+      width={100}
+      height={100}
+    />
+  );
+}
+export default function Profile() {
+
+  return (<Avatar person={{name:'Francis Osei Owusu',imageId:'1bX5QH6'}} size={100}/>)
+
+}

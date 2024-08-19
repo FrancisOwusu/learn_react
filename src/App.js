b54ab2a404a092e13ce7b059a93c3e131f7d8b2d
fowusu <osei1257@gmail.com>
2024-08-19 07:00:55 +0000

    refactor to my myapp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React, { useSate, useEffect } from 'react';
 import Gallery from "./Gallery";
 import Profile from "./Profile";
 import TodoList from "./TodoList";
 import SecondProfile from "./SecondProfile";
 import Avatar from "./Avatar";
+
 
 function MyButton() {
   return <button>I'm a button</button>;
@@ -11,9 +13,18 @@ function MyButton() {
 function Card({ children }) {
   return <div className="card">{children}</div>;
 }
-
+function useTime() {
+  const [time, setTime] = useState(() => new Date());
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+  return time;
+}
 export default function MyApp() {
-  return (
+ return (
     <div>
       <h1>Welcome to my app</h1>
       <h3>First Profile</h3>
@@ -31,6 +42,8 @@ export default function MyApp() {
         />
       </Card>
       <TodoList />
+      {/* Color management */}
+     
     </div>
   );
 }

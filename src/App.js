809ede17a6150342d15860f0a702261e915502dd
fowusu <osei1257@gmail.com>
2024-08-22 16:29:23 +0000

    add tenary condition to DrinkList

--- src/App.js
+++ src/App.js
@@ -24,6 +24,31 @@ function useTime() {
   }, []);
   return time;
 }
+
+function Drink({ name }) {
+  return (
+    <section>
+      <h1>{name}</h1>
+      <dl>
+        <dt>Part of plant</dt>
+        <dd>{name === 'tea' ? 'leaf' : 'bean'}</dd>
+        <dt>Caffeine content</dt>
+        <dd>{name === 'tea' ? '15–70 mg/cup' : '80–185 mg/cup'}</dd>
+        <dt>Age</dt>
+        <dd>{name === 'tea' ? '4,000+ years' : '1,000+ years'}</dd>
+      </dl>
+    </section>
+  );
+}
+
+ function DrinkList() {
+  return (
+    <div>
+      <Drink name="tea" />
+      <Drink name="coffee" />
+    </div>
+  );
+}
 export default function MyApp() {
  return (
     <div>
@@ -45,6 +70,7 @@ export default function MyApp() {
       <TodoList />
       {/* Color management */}
      <PackingList />
+     <DrinkList />
     </div>
   );
 }

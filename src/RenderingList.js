1d107181fcd75d5e7689f450bd73eca631e254c0
fowusu <osei1257@gmail.com>
2024-08-23 22:32:46 +0000

    add filter to ItemList  in renderingList

--- src/RenderingList.js
+++ src/RenderingList.js
@@ -1,3 +1,6 @@
+import { people } from "./data.js";
+import { getImageUrl } from "./util";
+
 const people1 = [
   "Creola Katherine Johnson: mathematician",
   "Mario José Molina-Pasquel Henríquez: chemist",
@@ -5,36 +8,24 @@ const people1 = [
   "Percy Lavon Julian: chemist",
   "Subrahmanyan Chandrasekhar: astrophysicist",
 ];
-const people = [
-  {
-    id: 0,
-    name: "Creola Katherine Johnson",
-    profession: "mathematician",
-  },
-  {
-    id: 1,
-    name: "Mario José Molina-Pasquel Henríquez",
-    profession: "chemist",
-  },
-  {
-    id: 2,
-    name: "Mohammad Abdus Salam",
-    profession: "physicist",
-  },
-  {
-    id: 3,
-    name: "Percy Lavon Julian",
-    profession: "chemist",
-  },
-  {
-    id: 4,
-    name: "Subrahmanyan Chandrasekhar",
-    profession: "astrophysicist",
-  },
-];
-
+/*
 export default function ItemList() {
-  const listItem = people.map((person) => <li>{person}</li>);
+  const listItem = people.map((person,index) => <li>{person}</li>);
 
   return listItem;
+}*/
+export default function ItemList() {
+  const chemist = people.filter((person) => person.profession === "chemist");
+  const listItems = chemist.map((person) => (
+    <li>
+      <img src={getImageUrl(person)} alt={person.name} />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  ));
+
+  return <ul>{listItems}</ul>;
 }

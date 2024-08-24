968a5b9c8b7698c30a71217cebe061eb460caeed
FrancisOwusu <osei1257@gmail.com>
2024-08-24 15:41:49 +0000

    add props to AlertButton in Button.js

--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,26 @@
+function AlertButton({ message, children }) {
+  return (
+    <button onClick={() => alert(message)}>
+      {children}
+    </button>
+  );
+}
+
+
+
 export default function Button() {
   function handleClick() {
     alert('You clicked me!');
   }
 
   return (
+    <>
+    <AlertButton message="Playing">Play Movie</AlertButton>
+    <AlertButton message="Uploading">Upload Image</AlertButton>
+    
     <button onClick={handleClick}>
       Click me
     </button>
+    </>
   );
 }

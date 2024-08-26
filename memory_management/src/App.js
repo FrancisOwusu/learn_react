cdbe21ca3e279fa8b665b2ba5a36e08ba59cc014
FrancisOwusu <osei1257@gmail.com>
2024-08-26 18:27:50 +0000

    add setState App js

--- memory_management/src/App.js
+++ memory_management/src/App.js
@@ -1,31 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { sculptureList } from "./data";
-function Gallery() {
-  let index = 0;
+import { useState } from "react";
+
+export default function App() {
+  const [index,setIndex] = useState(0);
+
   function handleClick() {
-    index = index + 1;
+    // index = index + 1;
+    setIndex(index + 1);
   }
-}
-function App() {
+
+  let sculpture = sculptureList[index];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>Next</button>
+      <h2>
+        <i>{sculpture.name}</i> by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img src={sculpture.url} alt={sculpture.alt}></img>
+      <p>{sculpture.description}</p>
     </div>
   );
 }
-
-export default App;

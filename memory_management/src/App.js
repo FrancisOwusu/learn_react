fcbd90ab17f4853b6c46cb761695666e61982da5
FrancisOwusu <osei1257@gmail.com>
2024-08-27 07:36:27 +0000

    added multiple state to component variables

--- memory_management/src/App.js
+++ memory_management/src/App.js
@@ -5,16 +5,25 @@ import { useState } from "react";
 
 export default function App() {
   const [index,setIndex] = useState(0);
+  const [showMore,setShowMore] = useState(false);
 
   function handleClick() {
     // index = index + 1;
     setIndex(index + 1);
+  }
+  function handleNextClick() {
+    setIndex(index + 1);
+  }
+
+  function handleMoreClick(){
+    setShowMore(!showMore);
   }
 
   let sculpture = sculptureList[index];
 
   return (
     <div className="App">
+    <h3>SET STATE USING SETINDEX</h3>
       <button onClick={handleClick}>Next</button>
       <h2>
         <i>{sculpture.name}</i> by {sculpture.artist}
@@ -22,6 +31,18 @@ export default function App() {
       <h3>
         ({index + 1} of {sculptureList.length})
       </h3>
+      <img src={sculpture.url} alt={sculpture.alt}></img>
+      <p>{sculpture.description}</p>
+
+      <h3>SET STATE USING SHOW MORE</h3>
+      <button onClick={handleNextClick}>Next</button>
+      <h2>
+        <i>{sculpture.name}</i> by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>{showMore? 'Hide':'Show'}details</button>{showMore && <p>{sculpture.description}</p>}
       <img src={sculpture.url} alt={sculpture.alt}></img>
       <p>{sculpture.description}</p>
     </div>
